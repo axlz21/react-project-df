220abe1f14736e8466c2b21d830b75e93a93cbf0
Aleksandar Dimitrov <axlzzz21@gmail.com>
2023-12-09 19:47:50 +0200

    Added Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import HeaderNav from './components/Header/Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
  
   return (
    <>
-   
+   <HeaderNav />
    <h1>Hello from react!</h1>
    </>
   )
