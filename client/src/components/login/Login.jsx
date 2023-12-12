03a87268b687a38707d5d357ec3d06d91de50ef0
Aleksandar Dimitrov <axlzzz21@gmail.com>
2023-12-12 20:38:32 +0200

    centered login form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -2,17 +2,19 @@ import "./login.css"
 import React from 'react';
 export default function Login(){
     return(
+        <div className="center-container">
         <div className="cover">
             <h1>Login</h1>
             <input type="text" placeholder="username"></input>
             <input type="password" placeholder="password"></input>
 
             <div className="login-btn">Login</div>
-            <p className="text">Or loing using</p>
+            <p className="text">Or login using</p>
             <div className="alt-login">
                 <div className="facebook"></div>
                 <div className="google"></div>
             </div>
+        </div>
         </div>
     )
 }
