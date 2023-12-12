1013bc54135a6df7bc802c729ee7aa1762322247
Aleksandar Dimitrov <axlzzz21@gmail.com>
2023-12-12 20:52:44 +0200

    add register form

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,10 +1,18 @@
-export default function Register(){
+import React from 'react';
+
+export default function Register() {
     return (
-        <div>
-            <h1>Register</h1>
-            <form>
-                
-            </form>
+        <div className="center-container">
+            <div className="cover">
+                <h1>Register</h1>
+                <input type="text" placeholder="username"></input>
+                <input type="text" placeholder="email"></input>
+                <input type="password" placeholder="password"></input>
+                <input type="repass" placeholder="password"></input>
+
+
+                <div className="login-btn">Register</div>
+            </div>
         </div>
-    );
+    )
 }
