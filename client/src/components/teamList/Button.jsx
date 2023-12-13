163d7ec17e9a3cd0540f826c9edca01d9ad0ad70
Aleksandar Dimitrov <axlzzz21@gmail.com>
2023-12-13 20:50:20 +0200

    Add join league button

---
+++ client/src/components/teamList/Button.jsx
@@ -0,0 +1,5 @@
+import "./teamlist.css"
+export default function Button(){
+
+    return(<button className="join-btn">Join league</button>);
+}
--- client/src/components/teamList/teamList.jsx
+++ client/src/components/teamList/teamList.jsx
@@ -1,15 +1,20 @@
+import Button from "./Button";
 import TeamCard from "./TeamCard";
 import "./teamlist.css"
-export default function TeamList(){
+export default function TeamList() {
     return (
         <div>
-            <h2 className="title">Teams:</h2>
-            <TeamCard />
-            <TeamCard />
-            <TeamCard />
-            <TeamCard />
-            <TeamCard />
+            
+            <h2 className="title">Teams: <Button /> </h2>
+            <div>
+                <TeamCard />
+                <TeamCard />
+                <TeamCard />
+                <TeamCard />
+                <TeamCard />
+                
+            </div>
         </div>
-        
+
     )
 }
