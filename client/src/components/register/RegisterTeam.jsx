29f5f6d1d8ce60ad59f5b0f7c792857d7a2f2c5b
Aleksandar Dimitrov <axlzzz21@gmail.com>
2023-12-18 19:55:02 +0200

    create team service

--- client/src/components/register/RegisterTeam.jsx
+++ client/src/components/register/RegisterTeam.jsx
@@ -1,17 +1,23 @@
-export default function RegisterTeam(){
-    return(
-        <div className="center-container">
-        <div className="cover">
-            <h1>Register team</h1>
-            <input type="text" placeholder="Team name"></input>
-            <input type="text" placeholder="Country"></input>
-            <input type="text" placeholder="City"></input>
-            <input type="text" placeholder="Phone number"></input>
-            <input type="text" placeholder="Logo URL"></input>
-            <input type="text-area" placeholder="Team description"></input>
-            <div className="join-btn">Join</div>
+export default function RegisterTeam() {
+    const registerTeamHandler = (e) => {
+        e.preventDefault()
+        const teamData = Object.fromEntries(new FormData(e.currentTarget));
+        
+    }
 
-        </div>
+    return (
+        <div className="center-container">
+            <form id="registerTeam" onSubmit={registerTeamHandler}>
+                <div className="cover">
+                    <h1>Register team</h1>
+                    <input type="text" name="teamName" placeholder="Team name"></input>
+                    <input type="text" name="city" placeholder="City"></input>
+                    <input type="text" name="phone" placeholder="Phone number"></input>
+                    <input type="text" name="logo" placeholder="Logo URL"></input>
+                    <input type="text-area" name="description" placeholder="Team description"></input>
+                    <input className="join-btn" type="submit" value="Join"></input>
+                </div>
+            </form>
         </div>
     )
 }
